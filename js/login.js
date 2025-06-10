12ce325a9b6cd57cfc299d0a47717c886021440d
Brayn-Meyer <coolmrepic9876@gmail.com>
2025-06-10 14:13:38 +0200

    Completed login page

--- js/login.js
+++ js/login.js
@@ -1,8 +1,9 @@
 const module = document.querySelector('.modal')
+const module_text = document.querySelector('p')
 const log_in_btn = document.querySelectorAll('.btn')[0]
 const try_again_btn = document.querySelectorAll('.btn')[1]
-const username = document.querySelector('.uname')
-const password = document.querySelector('.pwd')
+const username = document.querySelector('#uname')
+const password = document.querySelector('#pwd')
 
 display_module(false)
 function display_module(display){
@@ -14,24 +15,40 @@ function display_module(display){
 }
 
 function validateLogin(){
-    if (username.value == '' && password.value == ''){
-        if (username.value == ''){
-            alert('Invalid username!!!')
-        } 
-        if (password.value == ''){
-            alert('Invalid password!!!')
-        }
-    } else {
-            window.location.href = '../index.html'
+    if (!/^[a-zA-Z0-9]+$/.test(username.value)){
+        module_text.innerText = 'Username should only contain letters and numbers.'
+        display_module(true)
+        return
     }
+    if (username.value == '') {
+        module_text.innerText = 'Username field should not be empty.'
+        display_module(true)
+        return
+    }
+    if (password.value.length < 8){
+        module_text.innerText = 'Invalid password length. Must contain 8 or more characters.'
+        display_module(true)
+        return
+    }
+    if (password.value == ''){
+        module_text.innerText = 'Password field should not be empty.'
+        display_module(true)
+        return
+    }
+    if (/^[a-zA-Z0-9]+$/.test(password.value)){
+        module_text.innerText = 'Password should contain special characters. *^%&$#@'
+        display_module(true)
+        return
+    }
+    
+    window.location.href = '../index.html'
 }
-
 log_in_btn.addEventListener('click', function(event){
     event.preventDefault()
     console.log("asdasdabsdbasd")
 })
 
 try_again_btn.addEventListener('click', function(){
-    display_module(true)
+    display_module(false)
 })
 
