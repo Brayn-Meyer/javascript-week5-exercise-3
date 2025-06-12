0543772b14607c4fcc3c8eee931091ac6b952950
Brayn-Meyer <coolmrepic9876@gmail.com>
2025-06-12 10:11:21 +0200

    More frustration

--- js/index.js
+++ js/index.js
@@ -1,35 +1,99 @@
-const nav_slidebar = document.querySelector('.nac-slidebar')
+const nav_slidebar = document.querySelector('.nav-sidebar')
+const nav_list = document.querySelectorAll('.nav-sidebar ul, .nav-siddebar ul li span, .nav-sidebar ul li a')
+const nav_btn = document.querySelector('.btn-toggle-nav')
 
 let toggle = false
 function toggleNav(){
     if (toggle == false){
-        nav_anim(false)
-    } else {
         nav_anim(true)
+        toggle = true
+    } else {
+        nav_anim(false)
+        toggle = false
     }
 }
 
+// function nav_anim(reverse){
+//     let id = null
+//     clearInterval(id)
+//     id = setInterval(nav_anim_frame(reverse), 1)
+// }
 function nav_anim(reverse){
-    let id = null
-    clearInterval(id)
-    id = setInterval(nav_anim_frame(reverse), 1)
-}
-function nav_anim_frame(reverse){
     if (reverse == false){
-        let width = 50
-        if (width == 272){
-            clearInterval(id)
-        } else {
-            width++
-            nav_slidebar.style.width = width + 'px'
-        }
+        // let width = 50
+        // let opacity = 0
+        // let rotation = 0
+        // if (width == 272 && opacity == 1 && rotation == 90){
+        //     clearInterval(id)
+        // } else {
+            // if (width < 272){
+            //     width++
+            //     nav_slidebar.style.width = width + 'px'
+            // } else {
+            //     nav_slidebar.style.width = '272px'
+            // }
+        nav_slidebar.style.width = '50px'
+        nav_slidebar.style.transition = 'width 272px 1s'
+            // if (opacity < 1){
+                // opacity += 0.0045045045045045
+                // nav_list.style.opacity = opacity
+        nav_list.forEach(link =>{
+            link.style.visibility = 'visible'
+            link.style.opacity = '0'
+            link.style.transition = 'opacity 1 1s'
+        })
+            // }
+            // } else {
+            //     nav_list.style.opacity = 1
+            // }
+            // if (rotation < 90){
+            //     rotation += 2.466666666666667
+            //     nav_btn.style.rotation = rotation
+            //     nav
+            // } else {
+            //     nav_btn.style.rotation = '90'
+            // }
+        nav_btn.style.rotation = '0'
+        nav_btn.style.transition = 'rotation 90 1s'
+        // }
     } else {
-        let width = 272
-        if (width == 50){
-            clearInterval(id)
-        } else {
-            width--
-            nav_slidebar.style.width = width + 'px'
-        }
+        // let width = 272
+        // let opacity = 1
+        // let rotation = 90
+        // if (width == 50  && opacity == 0 && rotation == 0){
+        //     clearInterval(id)
+        // } else {
+            // if (width > 50){
+            //     width--
+            //     nav_slidebar.style.width = width + 'px'
+            // } else {
+            //     nav_slidebar.style.width = '50px'
+            // }
+            // if (opacity > 0){
+            //    opacity -= 0.0045045045045045
+            //    nav_list.style.opacity = opacity
+        nav_slidebar.style.width = '272px'
+        nav_slidebar.style.transition = 'width 50px 1s'
+
+        nav_list.forEach(link =>{
+            link.style.visibility = 'visible'
+            link.style.opacity = '1'
+            link.style.transition = 'opacity 0 1s'
+        })
+            // } else {
+            //     nav_list.style.opacity = 0
+            // }
+            // if (rotation > 0){
+            //     rotation -= 2.466666666666667
+            //     nav_btn.style.rotation = rotation
+            // } else {
+            //     nav_btn.style.rotation = '0'
+            // }
+        nav_btn.style.rotation = '90'
+        nav_btn.style.transition = 'rotation 0 1s'
+        // }
     }
+
+
+
 }
--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
-const module = document.querySelector('.modal')
-const module_text = document.querySelector('p')
+const modal = document.querySelector('.modal')
+const modal_text = document.querySelector('p')
 const log_in_btn = document.querySelectorAll('.btn')[0]
 const try_again_btn = document.querySelectorAll('.btn')[1]
 const username = document.querySelector('#uname')
@@ -8,35 +8,35 @@ const password = document.querySelector('#pwd')
 display_module(false)
 function display_module(display){
     if (display == false){
-        module.style.display = "none"
+        modal.style.display = "none"
     } else {
-        module.style.display = "block"
+        modal.style.display = "block"
     }
 }
 
 function validateLogin(){
     if (!/^[a-zA-Z0-9]+$/.test(username.value)){
-        module_text.innerText = 'Username should only contain letters and numbers.'
+        modal_text.innerText = 'Username should only contain letters and numbers.'
         display_module(true)
         return
     }
     if (username.value == '') {
-        module_text.innerText = 'Username field should not be empty.'
+        modal_text.innerText = 'Username field should not be empty.'
         display_module(true)
         return
     }
     if (password.value.length < 8){
-        module_text.innerText = 'Invalid password length. Must contain 8 or more characters.'
+        modal_text.innerText = 'Invalid password length. Must contain 8 or more characters.'
         display_module(true)
         return
     }
     if (password.value == ''){
-        module_text.innerText = 'Password field should not be empty.'
+        modal_text.innerText = 'Password field should not be empty.'
         display_module(true)
         return
     }
     if (/^[a-zA-Z0-9]+$/.test(password.value)){
-        module_text.innerText = 'Password should contain special characters. *^%&$#@'
+        modal_text.innerText = 'Password should contain special characters. *^%&$#@'
         display_module(true)
         return
     }
@@ -45,7 +45,7 @@ function validateLogin(){
 }
 log_in_btn.addEventListener('click', function(event){
     event.preventDefault()
-    console.log("asdasdabsdbasd")
+
 })
 
 try_again_btn.addEventListener('click', function(){
