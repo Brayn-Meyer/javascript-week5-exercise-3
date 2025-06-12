197257287534a24290bcd75719b2687607f0a383
Brayn-Meyer <coolmrepic9876@gmail.com>
2025-06-12 11:09:57 +0200

    Completed Exercise 3

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const nav_btn = document.querySelector('.btn-toggle-nav')
 
 let toggle = false
 function toggleNav(){
+    //Switches between the two animation states in nav_anim when function is triggered
     if (toggle == false){
         nav_anim(true)
         toggle = true
@@ -13,87 +14,37 @@ function toggleNav(){
     }
 }
 
-// function nav_anim(reverse){
-//     let id = null
-//     clearInterval(id)
-//     id = setInterval(nav_anim_frame(reverse), 1)
-// }
 function nav_anim(reverse){
+    //Checks if reverse is true in order to activate the correct animation.
     if (reverse == false){
-        // let width = 50
-        // let opacity = 0
-        // let rotation = 0
-        // if (width == 272 && opacity == 1 && rotation == 90){
-        //     clearInterval(id)
-        // } else {
-            // if (width < 272){
-            //     width++
-            //     nav_slidebar.style.width = width + 'px'
-            // } else {
-            //     nav_slidebar.style.width = '272px'
-            // }
+        //Smoothly changes the width of the slidebar.
         nav_slidebar.style.width = '50px'
         nav_slidebar.style.transition = 'width 272px 1s'
-            // if (opacity < 1){
-                // opacity += 0.0045045045045045
-                // nav_list.style.opacity = opacity
+
+        //Smoothly changes the opacity of each element in nav_list.
         nav_list.forEach(link =>{
             link.style.visibility = 'visible'
             link.style.opacity = '0'
-            link.style.transition = 'opacity 1 1s'
+            link.style.transition = 'opacity 1s'
         })
-            // }
-            // } else {
-            //     nav_list.style.opacity = 1
-            // }
-            // if (rotation < 90){
-            //     rotation += 2.466666666666667
-            //     nav_btn.style.rotation = rotation
-            //     nav
-            // } else {
-            //     nav_btn.style.rotation = '90'
-            // }
-        nav_btn.style.rotation = '0'
-        nav_btn.style.transition = 'rotation 90 1s'
-        // }
+
+        //Smoothly changes the rotation of the nav_btn.
+        nav_btn.style.transform = 'rotate(0deg)'
+        nav_btn.style.transition = 'transform rotate(90deg) 1s'
     } else {
-        // let width = 272
-        // let opacity = 1
-        // let rotation = 90
-        // if (width == 50  && opacity == 0 && rotation == 0){
-        //     clearInterval(id)
-        // } else {
-            // if (width > 50){
-            //     width--
-            //     nav_slidebar.style.width = width + 'px'
-            // } else {
-            //     nav_slidebar.style.width = '50px'
-            // }
-            // if (opacity > 0){
-            //    opacity -= 0.0045045045045045
-            //    nav_list.style.opacity = opacity
+        //Smoothly changes the width of the slidebar.
         nav_slidebar.style.width = '272px'
         nav_slidebar.style.transition = 'width 50px 1s'
 
+        //Smoothly changes the opacity of each element in nav_list.
         nav_list.forEach(link =>{
             link.style.visibility = 'visible'
             link.style.opacity = '1'
             link.style.transition = 'opacity 0 1s'
         })
-            // } else {
-            //     nav_list.style.opacity = 0
-            // }
-            // if (rotation > 0){
-            //     rotation -= 2.466666666666667
-            //     nav_btn.style.rotation = rotation
-            // } else {
-            //     nav_btn.style.rotation = '0'
-            // }
-        nav_btn.style.rotation = '90'
-        nav_btn.style.transition = 'rotation 0 1s'
-        // }
+
+        //Smoothly changes the rotation of the nav_btn.
+        nav_btn.style.transform = 'rotate(90deg)'
+        nav_btn.style.transition = 'transform rotate(0deg) 1s'
     }
-
-
-
 }
--- js/login.js
+++ js/login.js
@@ -5,8 +5,9 @@ const try_again_btn = document.querySelectorAll('.btn')[1]
 const username = document.querySelector('#uname')
 const password = document.querySelector('#pwd')
 
-display_module(false)
-function display_module(display){
+display_modal(false)
+function display_modal(display){
+    //Switches between whether or not to make the modal visible.
     if (display == false){
         modal.style.display = "none"
     } else {
@@ -15,40 +16,48 @@ function display_module(display){
 }
 
 function validateLogin(){
+    //Ensures the username field only contains numbers instead of any special characters.
     if (!/^[a-zA-Z0-9]+$/.test(username.value)){
         modal_text.innerText = 'Username should only contain letters and numbers.'
-        display_module(true)
+        display_modal(true)
         return
     }
+    //Ensures the username field isn't empty.
     if (username.value == '') {
         modal_text.innerText = 'Username field should not be empty.'
-        display_module(true)
+        display_modal(true)
         return
     }
+    //Ensures the password field is longer than 7 characters.
     if (password.value.length < 8){
         modal_text.innerText = 'Invalid password length. Must contain 8 or more characters.'
-        display_module(true)
+        display_modal(true)
         return
     }
+    //Ensures the password field isn't empty.
     if (password.value == ''){
         modal_text.innerText = 'Password field should not be empty.'
-        display_module(true)
+        display_modal(true)
         return
     }
+    //Ensures the password contains special characters.
     if (/^[a-zA-Z0-9]+$/.test(password.value)){
         modal_text.innerText = 'Password should contain special characters. *^%&$#@'
-        display_module(true)
+        display_modal(true)
         return
     }
     
+    //Relocates the user to another web page when all if statements are cleared.
     window.location.href = '../index.html'
 }
+//Prevents the page from reloading when submit button is pressed.
 log_in_btn.addEventListener('click', function(event){
     event.preventDefault()
 
 })
 
+//Disables the modal when the retry biutton is pressed.
 try_again_btn.addEventListener('click', function(){
-    display_module(false)
+    display_modal(false)
 })
 
